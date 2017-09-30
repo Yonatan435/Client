5947c102b5ddc04a7ab9c240097cefa239832f4c
Yonatan435 <32424107+Yonatan435@users.noreply.github.com>
2017-10-01 00:25:13 +0300

    Initial commit

---
+++ Client/Client.cs
@@ -0,0 +1,30 @@
+﻿using Client.ServerService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client: IServerCallback
+    {
+        List<int> _recievedNumbers;
+        List<byte> _recievedBytes;
+        public Client()
+        {
+            _recievedNumbers = new List<int>();
+            _recievedBytes = new List<byte>();
+        }
+        public void ReceiveInt(int number)
+        {
+            _recievedNumbers.Add(number);
+            Console.WriteLine("Recieved number: {0}", number);
+        }
+        public void ReceiveData(byte[] data)
+        {
+            _recievedBytes.AddRange(data);
+            Console.WriteLine("Recieved data");
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using Client.ServerService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        static InstanceContext site = new InstanceContext(new Client());
+        static ServerService.ServerClient proxy = new ServerService.ServerClient(site);
+        //static ServerServiceMex.ServerClient proxy = new ServerServiceMex.ServerClient(site);
+        static void Main(string[] args)
+        {
+            bool loggedIn = false;
+            while (!loggedIn)
+            {
+                Client Client = new Client();
+                Console.Write("Enter Login: ");
+                string login = Console.ReadLine();
+                Console.Write("Enter Password: ");
+                string password = Console.ReadLine();
+                loggedIn = proxy.Authenticate(login, password);
+            }
+            Console.WriteLine("Login Successful");
+            Console.WriteLine("Enter 'start' to start byte transfer, 'stop' to stop byte transfer, 'logout' to logout");
+            while (true)
+            {
+                Console.Write("Enter Command: ");
+                string command = Console.ReadLine();
+                if (command.Equals("start"))
+                    proxy.StartDataTransfer();
+                else if (command.Equals("stop"))
+                    proxy.StopDataTransfer();
+                else if (command.Equals("logout"))
+                {
+                    proxy.Logout();
+                    break;
+                }
+                else
+                    Console.WriteLine("Invalid command");
+            }
+        }
+    }
+}
